812acaacb81890d36ff156a61db52e47ba4f228e
John Sivak <jsivak@morpace.com>
2010-11-12 11:59:23 -0500

    Rename env var WORKIT_HOME to WORKIT_DIRS

--- workit.sh
+++ workit.sh
@@ -29,10 +29,10 @@ BASEDIR=`dirname $0`
 #source $BASEDIR/process_functions.sh
 
 # You can override this setting in your .zshrc/.bashrc
-if [ "$WORKIT_HOME" = "" ]
+if [ "$WORKIT_DIRS" = "" ]
 then
-    WORKIT_HOME=( "$HOME/src" "$HOME/configs" "$HOME/gitbox" )
-	export WORKIT_HOME
+    WORKIT_DIRS=( "$HOME/src" "$HOME/configs" "$HOME/gitbox" )
+	export WORKIT_DIRS
 fi
 
 # Normalize the directory name in case it includes 
@@ -40,18 +40,18 @@ fi
 # this broke hard for some reason so forget normalizing for now. 
 # it's probably some sort of subshell thing again, but 
 # for now just leave it be and look it up later
-# for ((i=1;i<=${#WORKIT_HOME};i++)); do
-#     rpath=$WORKIT_HOME[$i]
+# for ((i=1;i<=${#WORKIT_DIRS};i++)); do
+#     rpath=$WORKIT_DIRS[$i]
 #     echo $rpath
-#     WORKIT_HOME[$i]=$(/bin/zsh -c 'cd "$rpath"; pwd')
+#     WORKIT_DIRS[$i]=$(/bin/zsh -c 'cd "$rpath"; pwd')
 # done
-# export WORKIT_HOME
+# export WORKIT_DIRS
 
 ### Functions
 
 # Verify that the WORKON_HOME directory exists
 function verify_workit_home () {
-    for zpath in ${WORKIT_HOME[@]}; do
+    for zpath in ${WORKIT_DIRS[@]}; do
         if [ ! -d "$zpath" ]
         then
             echo "ERROR: projects directory '$zpath' does not exist." >&2
@@ -67,7 +67,7 @@ function verify_workit_project () {
     proj_count=0
     proj_list=()
 
-    for zpath in ${WORKIT_HOME[@]}; do
+    for zpath in ${WORKIT_DIRS[@]}; do
         target_path="$zpath/$env_name"
         if [[ -d $target_path ]]; then
             proj_list+=("$target_path")
@@ -125,7 +125,7 @@ function workit_run_hook () {
     fi
 }
 
-# Create a new project, in the WORKIT_HOME.
+# Create a new project, in the WORKIT_DIRS.
 #
 # Usage: mkworkit [options] PROJNAME
 function mkworkit () {
@@ -136,11 +136,11 @@ function mkworkit () {
         return 1
     fi
 
-    workit_home_count=${#WORKIT_HOME[*]}
+    workit_home_count=${#WORKIT_DIRS[*]}
 
     if [ $workit_home_count -gt 1 ]
     then
-        select proj_path in ${WORKIT_HOME[@]}
+        select proj_path in ${WORKIT_DIRS[@]}
         do
             case "$proj_path" in
                 *)
@@ -149,7 +149,7 @@ function mkworkit () {
             esac
         done
     else
-        proj_path=$WORKIT_HOME
+        proj_path=$WORKIT_DIRS
     fi
 
     eval "projname=\$$#"
@@ -181,7 +181,7 @@ function mkworkit () {
     # we should switch to the environment and run the hook.
     [ ! -d "$proj_path/$envname" ] && return 0
     workit "$projname"
-    #workit_source_hook "$WORKIT_HOME/postmkvirtualenv"
+    #workit_source_hook "$WORKIT_DIRS/postmkvirtualenv"
 }
 
 # List the available environments.
@@ -190,8 +190,8 @@ function show_workit_projects () {
     # NOTE: DO NOT use ls here because colorized versions spew control characters
     #       into the output list.
     all=()
-    for tpath in ${WORKIT_HOME[@]}; do
-        echo -e "Workit directory ${WORKIT_HOME[$i]}:"
+    for tpath in ${WORKIT_DIRS[@]}; do
+        echo -e "Workit directory ${WORKIT_DIRS[$i]}:"
         echo -e "----------------------------------------"
         ls --color=auto -C $tpath
         echo -e "\n"
@@ -201,8 +201,8 @@ function show_workit_projects () {
 # list the available workit home directories for adding a new project to
 function show_workit_home_options () {
     verify_workit_home || return 1
-    for ((i=1;i<${#WORKIT_HOME[*]};i++)); do
-        proj=${WORKIT_HOME[$i]}
+    for ((i=1;i<${#WORKIT_DIRS[*]};i++)); do
+        proj=${WORKIT_DIRS[$i]}
         echo "$i - $proj"
     done
 }
