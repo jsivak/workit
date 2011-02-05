378e71c2baad2e99c7fd979432fdd2e7d10ac9f7
John Sivak <jsivak@morpace.com>
2011-02-05 17:03:59 -0500

    Add support for "direct" paths to workit/mkworkit
      - Add support for "." to workit() and mkworkit().
      - Have the workitdone() script unset itself after it completes to
        better clean up after itself.
      - Add directory listing support to workit() when no project/directory is specified.
      - Allow direct paths to be specified for workit() and mkworkit().
        This allows workit to be used on any directory, not just ones
        in the WORKIT_DIRS list.
      - Remove some commented code.
      - Clean up whitespace issues.

--- workit.sh
+++ workit.sh
@@ -14,7 +14,7 @@
 #       [username] is the system username you logged in with
 #       [hostname] is the hostname of the system
 #
-# This allows a team of developers to work on the same project and have 
+# This allows a team of developers to work on the same project and have
 # differnt activation scripts to suit their own needs
 
 # Shell check; mitechie LOVES zsh, so we need to accommodate. :)
@@ -35,18 +35,6 @@ then
 	export WORKIT_DIRS
 fi
 
-# Normalize the directory name in case it includes 
-# relative path components.
-# this broke hard for some reason so forget normalizing for now. 
-# it's probably some sort of subshell thing again, but 
-# for now just leave it be and look it up later
-# for ((i=1;i<=${#WORKIT_DIRS};i++)); do
-#     rpath=$WORKIT_DIRS[$i]
-#     echo $rpath
-#     WORKIT_DIRS[$i]=$(/bin/zsh -c 'cd "$rpath"; pwd')
-# done
-# export WORKIT_DIRS
-
 ### Functions
 
 # Verify that the WORKON_HOME directory exists
@@ -66,6 +54,7 @@ function verify_workit_project () {
     proj_count=0
     proj_list=()
 
+    # Sanity chcks
     if [[ "${USER}" == "" ]]; then
         echo -e "Can't determine user name based on USER environment variable."
         return 1
@@ -75,6 +64,7 @@ function verify_workit_project () {
         echo -e "Can't determine hostname based on 'hostname -s' command."
         return 1
     fi
+
     for zpath in ${WORKIT_DIRS[@]}; do
         target_path="$zpath/$env_name"
         if [[ -d $target_path ]]; then
@@ -135,64 +125,95 @@ function workit_run_hook () {
     fi
 }
 
-# Create a new project, in the WORKIT_DIRS.
+# Create a new project, either based on a WORKIT_DIRS
+# or directly specified.
 #
-# Usage: mkworkit [options] PROJNAME
+# Usage: mkworkit [ directory_in_WORKIT_DIRS | path_to_directory ]
 function mkworkit () {
-    verify_workit_home || return 1
-
     if [[ "$1" == "" ]]; then
-        echo -e "\nUsage: mkworkit [project_name]\n"
+        echo -e "\nUsage: mkworkit [ directory_in_WORKIT_DIRS | path_to_directory ]\n"
         return 1
     fi
 
-    workit_home_count=${#WORKIT_DIRS[*]}
+    # check if we're given a direct path
+    if [[ "$1" == */* || "$1" == "." ]]; then
+        DIRECT=1
+        # Allow the current working directory to be specified.
+        if [[ "$1" == "." ]]; then
+            PROJ_PATH="$PWD"
+        else
+            PROJ_PATH="$1"
+        fi
 
-    if [ $workit_home_count -gt 1 ]
-    then
-        select proj_path in ${WORKIT_DIRS[@]}
-        do
-            case "$proj_path" in
-                *)
-                break
-                ;;
-            esac
-        done
+        if [[ -d "$PROJ_PATH" ]]; then
+            echo "workit: mkworkit setting up on direct path $PROJ_PATH"
+        else
+            echo "workit: Can't find $PROJ_PATH"
+            return 1
+        fi
     else
-        proj_path=$WORKIT_DIRS
+        DIRECT=0
+        verify_workit_home || return 1
+
+        workit_home_count=${#WORKIT_DIRS[*]}
+
+        if [ $workit_home_count -gt 1 ]
+        then
+            OLD_IFS=$IFS
+            # Remove the space from the IFS so we can show a "None / Cancel" option
+            IFS=$'\t\n'
+            DIR_LIST=('None / Cancel')
+            DIR_LIST+=("${WORKIT_DIRS[@]}")
+            select PROJ_PATH in ${DIR_LIST[@]}
+            do
+                case "$PROJ_PATH" in
+                    "None / Cancel")
+                        IFS=$OLD_IFS
+                        return 0
+                        ;;
+                    *)
+                        break
+                        ;;
+                esac
+            done
+            IFS=$OLD_IFS
+        else
+            PROJ_PATH=$WORKIT_DIRS
+        fi
+
+        PROJ_NAME="$1"
+        PROJ_PATH="$PROJ_PATH/$PROJ_NAME"
+        echo "workit: mkworkit setting up  $PROJ_NAME in WORKIT_DIRS path ${PROJ_PATH%/*}"
     fi
 
-    eval "projname=\$$#"
-
-    proj_workit_path="$proj_path/$projname"
-    build_workit_script_path "$proj_workit_path" 
+    build_workit_script_path "$PROJ_PATH" 
     SCRIPT_PATH=$RET_VAL
 
     # test for existing proj dir, if not create it, otherwise add 
-    # the post* script files to the existing dir
-    if [ ! -d $SCRIPT_PATH ]
-    then
-        (cd "$proj_path" &&
-        mkdir -p "$SCRIPT_PATH"
-        )
-    else
-        (cd "$SCRIPT_PATH")
-    fi
-
+    # the script files to the existing dir
     if [[ ! -d $SCRIPT_PATH ]]; then
         mkdir -p "$SCRIPT_PATH"
+        echo "workit: New scripts added to $SCRIPT_PATH in $PROJ_PATH"
+    else
+        echo "workit: Scripts already exist at $SCRIPT_PATH in $PROJ_PATH"
     fi
 
-    touch "${SCRIPT_PATH}/activate" &&
-    touch "${SCRIPT_PATH}/deactivate" &&
-    chmod +x "${SCRIPT_PATH}/activate" "${SCRIPT_PATH}/deactivate" 
+    FILES="activate deactivate"
+    for FILE in $FILES; do
+        if [[ ! -f "${SCRIPT_PATH}/$FILE" ]]; then
+            touch "${SCRIPT_PATH}/$FILE" 
+        fi
+        if [[ ! -x "${SCRIPT_PATH}/$FILE" ]]; then
+            chmod +x "${SCRIPT_PATH}/$FILE" 
+        fi
+    done
 
-    # If they passed a help option or got an error from virtualenv,
-    # the environment won't exist.  Use that to tell whether
-    # we should switch to the environment and run the hook.
-    [ ! -d "$proj_path/$envname" ] && return 0
-    workit "$projname"
-    #workit_source_hook "$WORKIT_DIRS/postmkvirtualenv"
+    # Now actually switch/workit into our new project
+    if [[ $DIRECT == 0 ]]; then
+        workit "$PROJ_NAME"
+    else
+        workit "$PROJ_PATH"
+    fi
 }
 
 # List the available environments.
@@ -200,8 +221,10 @@ function show_workit_projects () {
     verify_workit_home || return 1
     # NOTE: DO NOT use ls here because colorized versions spew control characters
     #       into the output list.
-    all=()
-    for tpath in ${WORKIT_DIRS[@]}; do
+    # Handle case where we just want to see the contents of 1 specified 
+    # directory.
+    if [[ "$1" != "" ]]; then
+        tpath=$1
         echo -e "Workit directory ${tpath}:"
         echo -e "----------------------------------------"
         if [[ -d $tpath ]]; then
@@ -210,7 +233,18 @@ function show_workit_projects () {
             echo -e "Directory ${tpath} doesn't exist"
         fi
         echo -e "\n"
-    done
+    else
+        for tpath in ${WORKIT_DIRS[@]}; do
+            echo -e "Workit directory ${tpath}:"
+            echo -e "----------------------------------------"
+            if [[ -d $tpath ]]; then
+                ls --color=auto -C $tpath
+            else
+                echo -e "Directory ${tpath} doesn't exist"
+            fi
+            echo -e "\n"
+        done
+    fi
 }
 
 # list the available workit home directories for adding a new project to
@@ -229,21 +263,69 @@ function show_workit_home_options () {
 function workit () {
 	PROJ_NAME="$1"
 
-	if [ "$PROJ_NAME" = "" ]
-    then
-        show_workit_projects
-        return 1
-    fi
-
-    if verify_workit_project "$PROJ_NAME" 
-    then
-        PROJ_PATH=$RET_VAL
-        export PROJ_PATH
+    # check if a path was provided; if so then just try to "workit" directly on that path
+    if [[ "$PROJ_NAME" == */* || "$PROJ_NAME" == "." ]]; then
+        # Allow the current working directory to be specified.
+        if [[ "$PROJ_NAME" == "." ]]; then
+            PROJ_NAME="$PWD"
+        fi
+        if [[ -d "$PROJ_NAME" ]]; then
+            echo "workit: activating direct path $PROJ_NAME"
+            PROJ_PATH="${PROJ_NAME}"
+        else
+            echo "workit: Can't find $PROJ_NAME"
+            return 1
+        fi
     else
-        return 1
-    fi
+        if [ "$PROJ_NAME" = "" ]
+        then
+            workit_home_count=${#WORKIT_DIRS[*]}
+            if [ $workit_home_count -gt 1 ]
+            then
+                OLD_IFS=$IFS
+                # Remove the space from the IFS so we can show a "None / Cancel" option
+                IFS=$'\t\n'
+                DIR_LIST=('None / Cancel')
+                DIR_LIST+=('All')
+                DIR_LIST+=("${WORKIT_DIRS[@]}")
+                echo "Select a directory to see available projects"
+                select PROJ_PATH in ${DIR_LIST[@]}
+                do
+                    case "$PROJ_PATH" in
+                        "None / Cancel")
+                            IFS=$OLD_IFS
+                            return 1
+                            ;;
+                        "All")
+                            unset PROJ_PATH
+                            break
+                            ;;
+                        *)
+                            break
+                            ;;
+                    esac
+                done
+                IFS=$OLD_IFS
+            else
+                PROJ_PATH=$WORKIT_DIRS
+            fi
 
-    verify_workit_home || return 1
+            show_workit_projects "$PROJ_PATH"
+            return 1
+        fi
+
+        if verify_workit_project "$PROJ_NAME" 
+        then
+            PROJ_PATH=$RET_VAL
+        else
+            return 1
+        fi
+
+        verify_workit_home || return 1
+
+        echo "workit: activating $PROJ_NAME in WORKIT_DIRS path ${PROJ_PATH%/*}"
+                                                           
+    fi
 
     # Deactivate any current environment "destructively"
     # before switching so we use our override function,
@@ -260,6 +342,7 @@ function workit () {
 
     eval 'function workitdone () {
         workit_source_hook "'$SCRIPT_PATH'/deactivate"
+        unset workitdone
     }'
     
     workit_source_hook "$SCRIPT_PATH/activate"
